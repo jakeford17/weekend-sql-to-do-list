786f1836be304d222ca9459244e04d1b014d60c8
Jake Ford <jakemford17@gmail.com>
2019-09-13 17:33:09 -0500

    Submit button logs out task entered

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,4 +4,13 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log("jQ");
+    $("#newTaskButton").on('click', submitNewTask);
+}
+
+function submitNewTask(){
+    console.log("SUBMIT BUTTON CLICKED");
+    let todo = {};
+    todo.task = $('#newTaskIn').val(); //keeping task inputted inside an object
+    console.log("Task Entered: ", todo)
+    // addTask(task);
 }
