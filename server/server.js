64d7588e26710e9425341a8628def347fa12c547
Jake Ford <jakemford17@gmail.com>
2019-09-13 17:08:31 -0500

    Initial Commit, created folders/files and sourced HTML

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const tasksRouter = require('./routes/task.router.js');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use('/tasks', tasksRouter);
+
+// Serve back static files by default
+app.use(express.static('server/public'))
+
+// Start listening for requests on a specific port
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log('listening on port', PORT);
+});
